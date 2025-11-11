90e654e56127f525dc460366dac7e80bbb69cecb
klotyks <208618052+klotyks@users.noreply.github.com>
2025-11-11 23:16:05 +0100

    Added: renders,generators, and modefied model

--- js/model.js
+++ js/model.js
@@ -1,4 +1,6 @@
 let users = []
+let posts = []
+let comments = []
 
 let user = {
   username: 'Bret',
@@ -7,6 +9,22 @@ let user = {
   website: 'hildegard.org',
 }
 
+let post = {
+  title: 'odio adipisci rerum aut animi',
+  body: 'quia molestiae reprehenderit quasi aspernaturnaut expedita occaecati aliquam eveniet laudantiumnomnis quibusdam delectus saepe quia accusamus maiores nam estncum et ducimus et vero voluptates excepturi deleniti ratione',
+}
+
+let comment = {
+  email: 'eliseo@gardner.biz',
+  body: 'harum non quasi et rationentempore iure ex voluptates in rationenharum architecto fugit inventore cupiditatenvoluptates magni quo et',
+}
+
 function addUser(user) {
   users.push(user)
+}
+function addPost(post) {
+  posts.push(post)
+}
+function addComment(comment) {
+  comments.push(comment)
 }
--- js/view.js
+++ js/view.js
@@ -1,9 +1,21 @@
-function renderUsers(users) {
+function renderComments(users) {
   elListUsers.innerHTML = ''
-  users.forEach(user => (elListUsers.innerHTML += generateUser(user)))
+  users.forEach(user => (elListUsers.innerHTML += generatePost(user)))
 }
 
-function generateUser(user) {
+function renderComments(posts) {
+  elListPosts.innerHTML = ''
+  posts.forEach(post => (elListPosts.innerHTML += generatePost(post)))
+}
+
+function renderComments(comments) {
+  elListComments.innerHTML = ''
+  comments.forEach(
+    comment => (elListComments.innerHTML += generateComment(comment))
+  )
+}
+
+function generatePost(user) {
   return `
 <li class="item-user">
   <div>
@@ -24,5 +36,33 @@ function generateUser(user) {
     <button>Get posts</button>
   </div>
 </li>
+`
+}
+function generatePost(post) {
+  return `
+<li class="item-post">
+    <span>
+      <b><i>${post.title}</i></b>
+    </span>
+    <span>
+      <b><i>${post.body}</i></b>
+    </span>
+    <div class="wrap-button">
+      <button>Get comments</button>
+    /div>
+  </li>
+`
+}
+
+function generateComment(comment) {
+  return `
+  <li class="item-comment">
+     <span>
+      <b><i>${comment.email}</i></b>
+     </span>
+     <span>
+      <b><i>${comment.body}</i></b>
+     </span>
+  </li>
 `
 }
