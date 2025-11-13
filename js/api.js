6b41f08fcdf1bc7d7c581b72bfca77666421dcb2
klotyks <208618052+klotyks@users.noreply.github.com>
2025-11-13 23:25:02 +0100

    Added: functions(getAllUsers and getPostsByUserId)

--- js/api.js
+++ js/api.js
@@ -1,7 +1,15 @@
 async function getAllUsers() {
-  // body
+  // const response = await fetch('https://jsonplaceholder.typicode.com/' + url)
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const text = await response.text()
+  return JSON.parse(text)
 }
 
 async function getPostsByUserId(userId) {
-  // body
+  // return await https://jsonplaceholder.typicode.com/posts?userId=4
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?userId=' + userId
+  )
+  const text = await response.text()
+  return JSON.parse(text)
 }
