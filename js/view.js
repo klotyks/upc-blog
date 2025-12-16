5d6e3c1a0e7ea409abddcb8fee3d0e79617179cc
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-12-17 00:55:37 +0200

    Migrated: to new template

--- js/view.js
+++ js/view.js
@@ -1,85 +1,38 @@
-function onClickButtonGetPosts(e) {
-  const elItemUser = e.target.closest('.item-user')
-  const userId = elItemUser.dataset['userId']
-  handleGetPostsByUserId(userId)
-}
-function onClickButtonGetAllUsers() {
-  handleGetUsersAll()
-}
-function onClickButtonGetComments(e) {
-  const elItemPost = e.target.closest('.item-post')
-  const postId = elItemPost.dataset['postId']
-  handleGetCommentsByPostId(postId)
-}
-
-function renderUsers(users) {
-  elListUsers.innerHTML = ''
-  users.forEach(user => (elListUsers.innerHTML += generateUser(user)))
-}
-
-function renderPosts(posts) {
-  elListPosts.innerHTML = ''
-  posts.forEach(post => (elListPosts.innerHTML += generatePost(post)))
-}
-
-function renderComments(comments) {
-  elListComments.innerHTML = ''
-  comments.forEach(
-    comment => (elListComments.innerHTML += generateComment(comment))
-  )
-}
-
-function generateUser(user) {
-  return `
-<li class="item-user" data-user-id=${user.id}>
-  <div>
-    <div>
-      <b>${user.nickname}</b>    
-    </div>
-    <div>
-      <i>${user.email}</i>
-    </div>
-    <div>
-      <span>${user.city}</span>
-    </div>
-    <div>
-      <u>${user.website}</u>
-    </div>
-  </div>
-  <div class="wrap-button">
-    <button onclick="onClickButtonGetPosts(event)">Get posts by userId</button>
-  </div>
-</li>
-`
-}
 function generatePost(post) {
   return `
-<li class="item-post" data-post-id=${post.id}>
-  <span>
-    <b><i>${post.title}</i></b>
-  </span>
-  <span>
-    <b><i>${post.body}</i></b>
-  </span>
-  <div class="wrap-button">
-    <button onclick="onClickButtonGetComments(event)">Get comments by postId</button>
+<div class="item-post">
+  <div class="post-content">
+    <h2>Шок!! Началось. Час назад совершилось не...</h2>
+    <p>
+      Час назад совершилось непридвиденное! Произошло то, чего никто не ожидал!
+    </p>
   </div>
-</li>
-`
+  <div>
+    <div class="wide">
+      <button>Подробней...</button>
+    </div>
+    <div class="post-info row">
+      <div>
+        <span>Автор:</span>
+        <b><i>klotyks</i></b>
+        <b>&nbsp;|&nbsp;</b>
+      </div>
+      <div>
+        <span>Комментариев:</span>
+        <b><i>3</i></b>
+        <b>&nbsp;|&nbsp;</b>
+      </div>
+      <div>
+        <span>Рейтинг: </span>
+        <span>
+          <span class="mdi mdi-star"></span>
+          <span class="mdi mdi-star"></span>
+          <span class="mdi mdi-star"></span>
+          <span class="mdi mdi-star-half-full"></span>
+          <span class="mdi mdi-star-outline"></span>
+        </span>
+      </div>
+    </div>
+  </div>
+</div>`
 }
-
-function generateComment(comment) {
-  return `
-<li class="item-comment">
-  <span>
-    <b><i>${comment.email}</i></b>
-  </span>
-  <span>
-    <b><i>${comment.body}</i></b>
-  </span>
-</li>
-`
-}
-
-const elButtonGetAllUsers = document.querySelector('#get-users')
-elButtonGetAllUsers.onclick = onClickButtonGetAllUsers
