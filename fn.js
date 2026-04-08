a2b7264eb9a62e2c8e91ae8bf9278d609ee11044
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2026-04-09 02:01:09 +0300

    Implemented: postPostFromUserId

---
+++ fn.js
@@ -0,0 +1,8 @@
+function a() {
+  console.log('i am A')
+}
+function b() {
+  console.log('i am B')
+}
+
+a(b())
--- js/model.js
+++ js/model.js
@@ -2,35 +2,41 @@ let users = []
 let posts = []
 let comments = []
 
-function createPost(dto, author, commentsCount) {
+function createPost(dto) {
   return {
-    id: dto.id,
-    userId: dto.userId,
+    id: Math.trunc(Math.random() * 10000),
     title: dto.title,
     body: dto.body,
-    author: author,
-    commentsCount: commentsCount,
-    rank: Math.trunc(Math.random()) * 5 + 1,
+    userId: dto.userId,
+    rank: Math.trunc(Math.random() * 5 + 1),
   }
 }
-
-function createComment() {
-  return {
-    userId: 1,
-    text: '',
-  }
-}
-
-function createUser() {
-  return {
-    id: 1,
-    nickname: '',
-  }
-}
-
-function addPost(dto, author, commentsCount) {
-  const post = createPost(dto, author, commentsCount)
+function addPost(dto) {
+  const post = createPost(dto)
   posts.push(post)
+}
+
+let x = Math.trunc(123.5)
+x = Math.random()
+x
+
+function createComment(dto) {
+  return {
+    id: Math.trunc(Math.random() * 10000),
+    userId: dto.user.id,
+    postId: dto.post.id,
+    text: dto.text,
+  }
+}
+function createUser(dto) {
+  return {
+    id: Math.trunc(Math.random() * 10000),
+    nickname: dto.nickname,
+  }
+}
+function addUser(dto) {
+  const user = createUser(dto)
+  users.push(user)
 }
 
 function setUsers(usersData) {
@@ -41,10 +47,6 @@ function setPosts(postsData) {
 }
 function setComments(commentsData) {
   comments = commentsData
-}
-
-function addUser(user) {
-  users.push(user)
 }
 
 function addComment(comment) {
@@ -62,3 +64,20 @@ function removeCommentById(id) {
 function removePostById(id) {
   posts = posts.filter(f => f.id !== id)
 }
+
+addUser({ nickname: 'Alex' })
+users
+
+function postPostFromUserId(userId) {
+  let dto = { title: 'Nuclear attack', body: 'Everything very bad', userId }
+  addPost(dto)
+}
+
+postPostFromUserId(users[0].id)
+posts
+
+// let dto2 = { text: 'omg... we`re doomed!', user: user, post: post }
+// let comment = createComment(dto2)
+
+// comment
+// posts
